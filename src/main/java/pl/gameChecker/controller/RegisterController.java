15d5c7e72771268b6962c3b3c3fdf41bb6da18f9
Michal Kabala <michal.kabala@steria.com>
2014-09-01 08:02:21 +0200

    added registration[on hold]

--- src/main/java/pl/gameChecker/controller/RegisterController.java
+++ src/main/java/pl/gameChecker/controller/RegisterController.java
@@ -1,6 +1,12 @@
 
 package pl.gameChecker.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -8,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import pl.gameChecker.model.hibernateEntities.Member;
 
 /**
  *
@@ -30,7 +37,20 @@ public class RegisterController {
             @RequestParam(value = "email", required = true) String email, 
             ModelMap model) {
         
-        //todo
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date registerDate = dateFormat.parse(dateFormat.format(date.getTime()));
+            Date bDay = dateFormat.parse(dateOfBirth);
+            java.sql.Date sqlRegisterDate = new java.sql.Date(registerDate.getTime());
+            java.sql.Date sqlBDay = new java.sql.Date(bDay.getTime());
+            Member newMember = new Member(login, password, sqlRegisterDate, email, sqlBDay);
+        } catch (ParseException ex) {
+            Logger.getLogger(RegisterController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         model.addAttribute("username", login);
         return "registerSuccess";
     }
